59686fc30f5bbc1c13581435a0a6425af23cf606
FelipePeralta01 <fe.peraltamiranda@gmail.com>
2023-11-09 19:08:37 -0300

    se protege todos los link con authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,59 +18,59 @@ const routes: Routes = [
   },
   {
     path: 'detalle-pedido',
-    loadChildren: () => import('./detalle-pedido/detalle-pedido.module').then( m => m.DetallePedidoPageModule)
+    loadChildren: () => import('./detalle-pedido/detalle-pedido.module').then( m => m.DetallePedidoPageModule), canActivate: [AuthGuard]
   },
   {
     path: 'product-add',
-    loadChildren: () => import('./producto/product-add/product-add.module').then( m => m.ProductAddPageModule)
+    loadChildren: () => import('./producto/product-add/product-add.module').then( m => m.ProductAddPageModule), canActivate: [AuthGuard]
   },
   {
     path: 'product-list',
-    loadChildren: () => import('./producto/product-list/product-list.module').then( m => m.ProductListPageModule)
+    loadChildren: () => import('./producto/product-list/product-list.module').then( m => m.ProductListPageModule), canActivate: [AuthGuard]
 },
 {
   path: 'product-detail/:id',
-  loadChildren: () => import('./producto/product-detail/product-detail.module').then( m => m.ProductDetailPageModule)
+  loadChildren: () => import('./producto/product-detail/product-detail.module').then( m => m.ProductDetailPageModule), canActivate: [AuthGuard]
 },
 {
   path: 'product-edit/:id',
-  loadChildren: () => import('./producto/product-edit/product-edit.module').then( m => m.ProductEditPageModule)
+  loadChildren: () => import('./producto/product-edit/product-edit.module').then( m => m.ProductEditPageModule), canActivate: [AuthGuard]
 },
 {
   path: 'product-all',
-  loadChildren: () => import('./producto/product-all/product-all.module').then( m => m.ProductAllPageModule)
+  loadChildren: () => import('./producto/product-all/product-all.module').then( m => m.ProductAllPageModule), canActivate: [AuthGuard]
 },
 {
   path: 'cliente-agregar',
-  loadChildren: () => import('./cliente/agregar/cliente.agregar.module').then( m => m.ClienteAgregarPageModule)
+  loadChildren: () => import('./cliente/agregar/cliente.agregar.module').then( m => m.ClienteAgregarPageModule), canActivate: [AuthGuard]
 },
 {
   path: 'cliente-actualizar',
-  loadChildren: () => import('./cliente/actualizar/cliente.actualizar.module').then( m => m.ClienteActualizarPageModule)
+  loadChildren: () => import('./cliente/actualizar/cliente.actualizar.module').then( m => m.ClienteActualizarPageModule), canActivate: [AuthGuard]
 },
 {
   path: 'cliente-actualizar/:id',
-  loadChildren: () => import('./cliente/actualizar/cliente.actualizar.module').then( m => m.ClienteActualizarPageModule)
+  loadChildren: () => import('./cliente/actualizar/cliente.actualizar.module').then( m => m.ClienteActualizarPageModule), canActivate: [AuthGuard]
 },
 {
   path: 'cliente-eliminar',
-  loadChildren: () => import('./cliente/eliminar/cliente.eliminar.module').then( m => m.ClienteEliminarPageModule)
+  loadChildren: () => import('./cliente/eliminar/cliente.eliminar.module').then( m => m.ClienteEliminarPageModule), canActivate: [AuthGuard]
 },
 {
   path: 'cliente-eliminar/:id',
-  loadChildren: () => import('./cliente/eliminar/cliente.eliminar.module').then( m => m.ClienteEliminarPageModule)
+  loadChildren: () => import('./cliente/eliminar/cliente.eliminar.module').then( m => m.ClienteEliminarPageModule), canActivate: [AuthGuard]
 },
 {
   path: 'cliente-leer',
-  loadChildren: () => import('./cliente/leer/cliente.leer.module').then( m => m.ClienteLeerPageModule)
+  loadChildren: () => import('./cliente/leer/cliente.leer.module').then( m => m.ClienteLeerPageModule), canActivate: [AuthGuard]
 },
 {
   path: 'cliente-leer/:id',
-  loadChildren: () => import('./cliente/leer/cliente.leer.module').then( m => m.ClienteLeerPageModule)
+  loadChildren: () => import('./cliente/leer/cliente.leer.module').then( m => m.ClienteLeerPageModule), canActivate: [AuthGuard]
 },
 {
   path: 'cliente-listar',
-  loadChildren: () => import('./cliente/listar/cliente.listar.module').then( m => m.ClienteListarPageModule)
+  loadChildren: () => import('./cliente/listar/cliente.listar.module').then( m => m.ClienteListarPageModule), canActivate: [AuthGuard]
 }
 ];
 
